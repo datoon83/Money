1f0711f185f8273c67c737b370e720e4e5ba76c3
datoon83 <datoon83@gmail.com>
2015-01-15 09:19:55 +0000

    addition of money class and some tests

---
+++ src/Money/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Money
+{
+    public class Money
+    {
+        public decimal Amount { get; private set; }
+        public string CurrencyCode { get; private set; }
+
+        public Money(decimal amount, string currencyCode = "GBP")
+        {
+            Amount = amount;
+            CurrencyCode = currencyCode;
+        }
+
+        public static implicit operator Money(decimal amount)
+        {
+            return new Money(amount);
+        }
+
+        public static bool operator >(Money firstAmount, Money secondAmount)
+        {
+            if (firstAmount.CurrencyCode != secondAmount.CurrencyCode)
+                throw new InvalidOperationException("Cannot compare different currency types!");
+
+            return firstAmount.Amount > secondAmount.Amount;
+        }
+
+        public static bool operator <(Money firstAmount, Money secondAmount)
+        {
+            if (firstAmount.CurrencyCode != secondAmount.CurrencyCode)
+                throw new InvalidOperationException("Cannot compare different currency types!");
+
+            return firstAmount.Amount < secondAmount.Amount;
+        }
+
+        public static Money operator +(Money firstAmount, Money secondAmount)
+        {
+            if (firstAmount.CurrencyCode != secondAmount.CurrencyCode)
+                throw new InvalidOperationException("Cannot add two different currency types!");
+
+            return new Money(firstAmount.Amount + secondAmount.Amount, firstAmount.CurrencyCode);
+        }
+
+        public static Money operator -(Money firstAmount, Money secondAmount)
+        {
+            if (firstAmount.CurrencyCode != secondAmount.CurrencyCode)
+                throw new InvalidOperationException("Cannot subtract two different currency types!");
+
+            return new Money(firstAmount.Amount - secondAmount.Amount, firstAmount.CurrencyCode);
+        }
+
+        public static Money operator *(Money firstAmount, Money secondAmount)
+        {
+            if (firstAmount.CurrencyCode != secondAmount.CurrencyCode)
+                throw new InvalidOperationException("Cannot multiply two different currency types!");
+
+            return new Money(firstAmount.Amount * secondAmount.Amount, firstAmount.CurrencyCode);
+        }
+
+        public static Money operator /(Money firstAmount, Money secondAmount)
+        {
+            if (firstAmount.CurrencyCode != secondAmount.CurrencyCode)
+                throw new InvalidOperationException("Cannot divide two different currency types!");
+
+            return new Money(firstAmount.Amount / secondAmount.Amount, firstAmount.CurrencyCode);
+        }
+    }
+}
